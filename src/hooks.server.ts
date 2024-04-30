4fca89fa0c75952488c712ac062fcce34baadc88
Hilmar Palsson <hilmar90@gmail.com>
2024-04-30 09:08:44 +0100

    revert change

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,7 +11,7 @@ import type { Handle } from '@sveltejs/kit';
 initLbShutdown();
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (event.url.pathname === '/' || event.url.pathname === '/logout') {
+	if (event.url.pathname === '/logout') {
 		return await resolve(event);
 	}
 
